f182eafaf2a2e1b78296761dc6d593b9a878d343
TheSilvaProf <fabio01@protonmail.ch>
2023-01-13 14:31:10 -0500

    Com mudancas

--- agenda.rb
+++ agenda.rb
@@ -17,6 +17,8 @@ loop do
 
 puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
     codigo = gets.chomp.to_i
+    puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
+    codigo = gets.chomp.to_i
 
     case
     when codigo == 0
