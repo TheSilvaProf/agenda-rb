b91973278dfd6a341e931668ee38faf0bf1d0613
TheSilvaProf <fabio01@protonmail.ch>
2023-01-30 07:05:14 -0500

    Agenda teh agora sem erro

---
+++ agenda2.rb
@@ -0,0 +1,28 @@
+agenda = {
+    Anna: "1111-1111",
+    Pedro: "1111-2222"
+}
+
+while true
+    puts ""
+    puts "[1] Adicionar"
+    puts "[2] Atualizar"
+    puts "[3] Exibir"
+    puts "[4] Excluir"
+    
+    print "Escolha uma opcao: "
+    escolha = gets.chomp()
+    puts ""
+
+    case escolha
+    when "1" 
+        print "Insira o nome do contato: "
+        nome = gets.chomp()
+        if agenda
+        
+    when 
+        
+    else
+        
+    end
+end
